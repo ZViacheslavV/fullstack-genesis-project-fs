a07985c74ecf6b774c332bc6a933573ff2f5805b
Oleksandra <olexandra.momotiuk@gmail.com>
2026-01-10 01:29:15 +0200

    temp functions and layout

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -1,6 +1,7 @@
-import { getMe } from "@/lib/api/serverApi";
+
+import ProfileEditForm from "@/components/profile/ProfileEditForm/ProfileEditForm";
+import { getServerMe } from "@/lib/api/serverApi";
 import { Metadata } from "next";
-import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Profile",
@@ -21,48 +22,15 @@ export const metadata: Metadata = {
 };
 
 
-
-
 async function ProfilePage() {
-  const user = await getMe();
-
-  const handleClick = () => {
-
-  };
-  const handleSubmit = (formData: FormData) => {
-
-	  console.log(`sent ${formData}`);
-  };
-
-  return (<div>
-    <div>
-    <Image
-        src={user.photo}
-        alt="User Avatar"
-        width={120}
-        height={120}
-      />
-    <p className="">username</p>
-    <p className="">email</p>
+  const user = await getServerMe();
+  return (<>
+    <div className="">
+      <p className="">{ user.name}</p>
+      <p className="">{ user.email}</p>
     </div>
-    <button className="" onClick={handleClick} >update picture</button>
-    <form action={handleSubmit} className="">
-      <label htmlFor="userName" className="">
-  <input defaultValue={user.name} type="text" id="userName" />
-      </label>
-      <label htmlFor="userEmail" className="">
-  <input  type="text"  id="userEmail" defaultValue={user.email}  />
-      </label>
-      <label htmlFor="babyGender" className="">
-  <input  type="select" id="babyGender" defaultValue={user.childGender} />
-      </label>
-      <label htmlFor="babyDueDate" className="">
-  <input type="select" id="babyDueDate" defaultValue={user.dueDate} />
-      </label>
-      <button type="submit" className="" >save changes</button>
-      <button className="" onClick={() => 'refresh'} >refresh</button>
-    </form>
-  </div>);
+    <ProfileEditForm user={user} />
+  </>);
 };
 
 export default ProfilePage;
--- components/profile/ProfileEditForm/ProfileEditForm.tsx
+++ components/profile/ProfileEditForm/ProfileEditForm.tsx
@@ -1,11 +1,61 @@
 'use client';
 
+import { User } from '@/types/user';
 import css from './ProfileEditForm.module.css';
+import { useState } from 'react';
 
 //===========================================================================
 
-function ProfileEditForm() {
-  return <div className={css.picker}></div>;
+interface ProfileEditFormProps {
+  user: User;
+}
+
+function ProfileEditForm({ user }: ProfileEditFormProps) {
+
+const [avatarFile, setAvatarFile] = useState<File | null>(null);
+
+
+ const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+   e.preventDefault();
+  setAvatarFile(null);
+  const form = e.currentTarget.form;
+  if (form) form.reset();
+};
+
+  const handleSubmit = (formData: FormData) => {
+       if (avatarFile) {
+      formData.append('avatar', avatarFile);
+    }
+     const payload = {
+      name: formData.get('name'),
+      email: formData.get('email'),
+      gender: formData.get('gender'),
+       dueDate: formData.get('dueDate'),
+      photo: avatarFile,
+    };
+//add send to server patch.user 
+	  console.log(`sent ${payload}`);
+  };
+
+  return <div className={css.picker}>
+  <form action={handleSubmit} className="">
+      <label htmlFor="userName" className="">
+  <input defaultValue={user.name} type="text" id="userName" />
+      </label>
+      <label htmlFor="userEmail" className="">
+  <input  type="text"  id="userEmail" defaultValue={user.email}  />
+      </label>
+      <label htmlFor="babyGender" className="">
+  <input  type="select" id="babyGender" defaultValue={user.gender} />
+      </label>
+      <label htmlFor="babyDueDate" className="">
+  <input type="select" id="babyDueDate" defaultValue={user.dueDate} />
+      </label>
+      <button className="" onClick={handleClick} >cancel changes</button>
+       <button type="submit" className="" >save changes</button>
+    </form>
+
+  </div>;
 }
 
 export default ProfileEditForm;
