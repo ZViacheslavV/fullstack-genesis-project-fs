abb51106f425712f079b1d63319037275b097000
ZVyachelsavV <vvvv.zkv@gmail.com>
2026-01-09 23:10:43 +0200

    add temporary auth status and api functions

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -4,6 +4,26 @@ import axios, { type AxiosInstance } from 'axios';
 
 const BASE_URL = `${process.env.NEXT_PUBLIC_API_URL}/api`;
 
+export const API_ENDPOINTS = {
+  LOGIN: '/auth/login',
+  REGISTER: '/auth/register',
+  LOGOUT: '/auth/logout',
+  REFRESH: '/auth/refresh',
+
+  USER_CURRENT_GET: '/users/current',
+  USER_CURRENT_PATCH: '/users/current',
+  USER_CURRENT_PATCH_AVA: '/users/current/avatar',
+
+  DIARIES_GET: '/diaries',
+  DIARIES_POST: '/diaries',
+  DIARIES_DELETE_ID: '/diaries/',
+  DIARIES_PATCH_ID: '/diaries/',
+
+  TASKS_GET: '/tasks',
+  TASKS_POST: '/tasks',
+  TASKS_PATCH_TASK_ID: '/tasks/',
+};
+
 export const nextServer: AxiosInstance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
