99c70040be7f4a9a01b034840432d9805452c506
Oleksandra <olexandra.momotiuk@gmail.com>
2026-01-09 23:39:58 +0200

    Temp layout, wait for auth fix

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -1,5 +1,68 @@
-const ProfilePage = () => {
-  return <div />;
+import { getMe } from "@/lib/api/serverApi";
+import { Metadata } from "next";
+import Image from "next/image";
+
+export const metadata: Metadata = {
+  title: "Profile",
+  description: "User profile",
+  openGraph: {
+    title: 'Profile',
+    description: 'User profile.',
+    url: '/',
+    images: [
+      {
+        url: '/',
+        width: 1200,
+        height: 630,
+        alt: 'Profile',
+      },
+    ],
+  },
+};
+
+
+
+
+async function ProfilePage() {
+  const user = await getMe();
+
+  const handleClick = () => {
+
+  };
+  const handleSubmit = (formData: FormData) => {
+
+	  console.log(`sent ${formData}`);
+  };
+
+  return (<div>
+    <div>
+    <Image
+        src={user.photo}
+        alt="User Avatar"
+        width={120}
+        height={120}
+      />
+    <p className="">username</p>
+    <p className="">email</p>
+    </div>
+    <button className="" onClick={handleClick} >update picture</button>
+    <form action={handleSubmit} className="">
+      <label htmlFor="userName" className="">
+  <input defaultValue={user.name} type="text" id="userName" />
+      </label>
+      <label htmlFor="userEmail" className="">
+  <input  type="text"  id="userEmail" defaultValue={user.email}  />
+      </label>
+      <label htmlFor="babyGender" className="">
+  <input  type="select" id="babyGender" defaultValue={user.childGender} />
+      </label>
+      <label htmlFor="babyDueDate" className="">
+  <input type="select" id="babyDueDate" defaultValue={user.dueDate} />
+      </label>
+      <button type="submit" className="" >save changes</button>
+      <button className="" onClick={() => 'refresh'} >refresh</button>
+    </form>
+  </div>);
 };
 
 export default ProfilePage;
--- components/profile/ProfileAvatar/ProfileAvatar.tsx
+++ components/profile/ProfileAvatar/ProfileAvatar.tsx
@@ -1,11 +1,70 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import css from './ProfileAvatar.module.css';
+import Image from 'next/image';
 
-//===========================================================================
 
-function ProfileAvatar() {
-  return <div className={css.picker}></div>;
+type Props = {
+  profilePhotoUrl?: string;
+};
+
+
+function ProfileAvatar({ profilePhotoUrl }: Props) {
+const [previewUrl, setPreviewUrl] = useState<string>('');
+  const [error, setError] = useState<string>('');
+
+useEffect(() => {
+    if (profilePhotoUrl) {
+      setPreviewUrl(profilePhotoUrl);
+    }
+  }, [profilePhotoUrl]);
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    setError('');
+
+    if (file) {
+      if (!file.type.startsWith('image/')) {
+        setError('Only images');
+        return;
+      }
+
+      if (file.size > 5 * 1024 * 1024) {
+        setError('Max file size 5MB');
+        return;
+      }
+
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreviewUrl(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+const handleRemove = () => {
+    setPreviewUrl('');
+  };
+
+
+
+  return <div className={css.picker}>
+    {previewUrl ? (
+      <Image src={previewUrl} alt='Preview' width={300} height={300} />
+    ) :
+      (<label htmlFor="avatar" >
+        Avatar
+        <input id="avatar" type="file" accept="image/*" onChange={handleFileChange} />
+      </label>)}
+    
+    {previewUrl && (
+          <button onClick={handleRemove}>
+            x
+          </button>
+        )}
+     {error && <p>{error}</p>}
+  </div>;
 }
 
 export default ProfileAvatar;
