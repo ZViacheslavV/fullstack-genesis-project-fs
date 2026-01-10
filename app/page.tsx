10cfdc23ad323950bd80c1580c967f091df6ebf9
Anna Polisciuc <annap00775@gmail.com>
2026-01-10 13:32:51 +0200

    Dashboard UI: Status, BabyToday, MomTip cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from 'next';
 import css from './page.module.css';
+import StatusBlock from '@/components/dashboard/StatusBlock/StatusBlock';
+import BabyTodayCard from '@/components/dashboard/BabyTodayCard/BabyTodayCard';
+import MomTipCard from '@/components/dashboard/MomTipCard/MomTipCard';
 
 //===========================================================================
 
@@ -8,13 +11,21 @@ export const metadata: Metadata = {};
 //===========================================================================
 
 function Home() {
+  const tipText =
+  'Не забувайте про зволоження шкіри живота та стегон спеціальними олійками, щоб попередити появу розтяжок.';
+
   return (
     <section className={css.hero}>
       <div>
         {/*<GreetingBlock />*/}
-        {/*<StatusBlock />*/}
-        {/*<BabyTodayCard />*/}
-        {/*<MomTipCard />*/}
+        { <StatusBlock currentWeek={14} daysLeft={165} /> }
+        { <BabyTodayCard
+  imageUrl="/images/avocado.png"
+  sizeText="Ваш малюк зараз розміром з авокадо"
+  description="Його кісточки починають тверднути, а рухи стають більш скоординованими."
+/>
+}
+        <MomTipCard text={tipText} /> 
       </div>
       <div>
         {/*<TasksReminderCard />*/}
