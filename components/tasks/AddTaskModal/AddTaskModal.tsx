d5cef749d6269da57e0d8c21c0b8d03e85a85343
Maksym Sirenko <toyotasan@gmail.com>
2026-01-10 18:44:25 +0200

    Fix AddTaskModal

--- components/tasks/AddTaskModal/AddTaskModal.tsx
+++ components/tasks/AddTaskModal/AddTaskModal.tsx
@@ -1,11 +1,26 @@
 'use client';
 
+import Modal from '@/components/common/Modal/Modal';
+import AddTaskForm from '@/components/tasks/AddTaskForm/AddTaskForm';
 import css from './AddTaskModal.module.css';
 
-//===============================================================
+type AddTaskModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+};
 
-function AddTaskModal() {
-  return <div className={css.backdrop}></div>;
+export default function AddTaskModal({ isOpen, onClose }: AddTaskModalProps) {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      showCloseButton
+      closeOnBackdrop
+      closeOnEsc
+    >
+      <div className={css.wrapper}>
+        <AddTaskForm afterSubmit={onClose} />
+      </div>
+    </Modal>
+  );
 }
-
-export default AddTaskModal;
