8a7852d8311b4ab7bf6df3e106c1d6f06861ae80
Viacheslav Zykov <vvvv.zkv@gmail.com>
2026-01-11 20:53:26 +0200

    Revert "paralel-routes"

--- app/(private routes)/journey/[weekNumber]/page.tsx
+++ app/(private routes)/journey/[weekNumber]/page.tsx
@@ -1,32 +1,12 @@
-export type JourneyPageProps = {
-  weekNumber: string;
+import JourneyDetails from '@/components/journey/JourneyDetails/JourneyDetails';
+
+type Props = {
+  params: { weekNumber: string };
 };
 
-export default function JourneyPage({ weekNumber }: JourneyPageProps) {}
+export default function JourneyPage({ params }: Props) {
+  const week = Number(params.weekNumber);
+  return <JourneyDetails weekNumber={week} />;
+}
 
-// import FeelingCheckCard from '@/components/dashboard/FeelingCheckCard/FeelingCheckCard';
-// import TasksReminderCard from '@/components/tasks/TasksReminderCard/TasksReminderCard';
-// import ComfortTips, {
-//   ComfortTipsProps,
-// } from '@/components/journey/ComfortTips/ComfortTips';
-// import { getJourneyData } from '@/lib/api/clientApi';
-
-// type Props = {
-//   params: { weekNumber: string };
-// };
-
-// const MomyJourney = async ({ params }: Props) => {
-//   const week = Number(params.weekNumber);
-
-//   const data = await getJourneyData(week, 'momy');
-
-//   return (
-//     <>
-//       <FeelingCheckCard />
-//       <ComfortTips comfortTips={data.comfortTips as ComfortTipsProps[]} />
-//       <TasksReminderCard />
-//     </>
-//   );
-// };
-
-// export default MomyJourney;
+///dont render tooo
