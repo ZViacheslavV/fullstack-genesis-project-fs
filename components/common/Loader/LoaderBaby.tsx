c431a79b13bf38031b0929f8c1e843e16f1f12f3
Vitaliy_Orlov <alpachino84@gmail.com>
2026-01-11 14:25:57 +0200

    refactor loader

--- components/common/Loader/LoaderBaby.tsx
+++ components/common/Loader/LoaderBaby.tsx
@@ -1,43 +1,84 @@
-import React from 'react';
-import { Player } from '@lottiefiles/react-lottie-player';
+// import React from 'react';
+// import { Player } from '@lottiefiles/react-lottie-player';
+
+// interface LoaderProps {
+//   size?: 'sm' | 'md' | 'lg';
+//   fullscreen?: boolean;
+// }
+
+// const Loader: React.FC<LoaderProps> = ({ size = 'md', fullscreen = false }) => {
+//   const sizeClasses = {
+//     sm: 'w-16 h-16',
+//     md: 'w-32 h-32',
+//     lg: 'w-64 h-64',
+//   };
+
+//   if (fullscreen) {
+//     return (
+//       <div className="fixed inset-0 bg-white/80 dark:bg-gray-900/80 flex items-center justify-center z-50">
+//         <div className={sizeClasses[size]}>
+//           <Player
+//             autoplay
+//             loop
+//             src="/public/loader.json"
+//             style={{ width: '100%', height: '100%' }}
+//           />
+//         </div>
+//       </div>
+//     );
+//   }
+
+//   return (
+//     <div className={`${sizeClasses[size]} mx-auto`}>
+//       <Player
+//         autoplay
+//         loop
+//         src="/public/loader.json"
+//         style={{ width: '100%', height: '100%' }}
+//       />
+//     </div>
+//   );
+// };
+
+// export default Loader;
+
+'use client';
+
+import dynamic from 'next/dynamic';
+
+const Player = dynamic(
+  () => import('@lottiefiles/react-lottie-player').then((m) => m.Player),
+  { ssr: false }
+);
 
 interface LoaderProps {
   size?: 'sm' | 'md' | 'lg';
   fullscreen?: boolean;
 }
 
-const Loader: React.FC<LoaderProps> = ({ size = 'md', fullscreen = false }) => {
-  const sizeClasses = {
-    sm: 'w-16 h-16',
-    md: 'w-32 h-32',
-    lg: 'w-64 h-64',
-  };
+const sizeMap = {
+  sm: 'w-16 h-16',
+  md: 'w-32 h-32',
+  lg: 'w-64 h-64',
+};
+
+export default function LoaderBaby({
+  size = 'md',
+  fullscreen = false,
+}: LoaderProps) {
+  const player = (
+    <div className={sizeMap[size]}>
+      <Player autoplay loop src="/loader.json" />
+    </div>
+  );
 
   if (fullscreen) {
     return (
-      <div className="fixed inset-0 bg-white/80 dark:bg-gray-900/80 flex items-center justify-center z-50">
-        <div className={sizeClasses[size]}>
-          <Player
-            autoplay
-            loop
-            src="/public/loader.json"
-            style={{ width: '100%', height: '100%' }}
-          />
-        </div>
+      <div className="fixed inset-0 flex items-center justify-center bg-white/80 z-50">
+        {player}
       </div>
     );
   }
 
-  return (
-    <div className={`${sizeClasses[size]} mx-auto`}>
-      <Player
-        autoplay
-        loop
-        src="/public/loader.json"
-        style={{ width: '100%', height: '100%' }}
-      />
-    </div>
-  );
-};
-
-export default Loader;
+  return player;
+}
