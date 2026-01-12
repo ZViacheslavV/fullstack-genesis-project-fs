39961b3ab5201a39aa11bec396ca428ea0fe5948
ZVyachelsavV <vvvv.zkv@gmail.com>
2026-01-12 15:54:48 +0200

    elec prev push

--- components/dashboard/DashboardCardClient/DashboardCardClient.tsx
+++ components/dashboard/DashboardCardClient/DashboardCardClient.tsx
@@ -7,9 +7,16 @@ import { useQuery } from '@tanstack/react-query';
 import LoaderStork from '@/components/common/Loader/LoaderStork';
 import BabyTodayCard from '@/components/dashboard/BabyTodayCard/BabyTodayCard';
 import { useAuthUserStore } from '@/lib/store/authStore';
+import { useEffect, useState } from 'react';
 
-export default function DashboardCardClient() {
+type Props = {
+  onMomDailyTip?: (tip: string) => void;
+};
+
+export default function DashboardCardClient({ onMomDailyTip }: Props) {
   const isAuthenticated = useAuthUserStore((s) => s.isAuthenticated);
+
+  const [modDailyTip, setMomDailyTip] = useState<string>('');
 
   const { data, isLoading, error } = useQuery({
     queryKey: ['weeks'],
@@ -27,6 +34,15 @@ export default function DashboardCardClient() {
 
   const mom = weeksInfo.momState;
   const tips = mom.comfortTips;
+
+  /*   useEffect(() => {
+    if (!baby?.momDailyTips) return;
+
+    const index = (new Date().getDay() + 6) % 7;
+    const tip = baby.momDailyTips[index];
+
+    onMomDailyTip?.(tip);
+  }, [baby?.momDailyTips, onMomDailyTip]); */
 
   const tipText =
     tips && tips.length > 0
