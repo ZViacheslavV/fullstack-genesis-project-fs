0a99feeac89411f23051e4e95cec9d868515ccc5
Yevheniia Serhieieva <y.o.serhieieva@gmail.com>
2026-01-14 13:48:49 +0200

    mobile

--- app/(private routes)/(navigation)/journey/[weekNumber]/page.tsx
+++ app/(private routes)/(navigation)/journey/[weekNumber]/page.tsx
@@ -5,8 +5,6 @@ import { useParams, notFound } from 'next/navigation';
 import BabyJourney from '@/components/journey/BabyJourney/BabyJourney';
 import JourneyDetails from '@/components/journey/JourneyDetails/JourneyDetails';
 import MomyJourney from '@/components/journey/MomyJourney/MomyJourney';
-import GreetingBlock from '@/components/diary/GreetingBlock/GreetingBlock';
-import WeekSelector from '@/components/journey/WeekSelector/WeekSelector';
 
 export default function JourneyPage() {
   const { weekNumber } = useParams<{ weekNumber: string }>();
@@ -17,9 +15,11 @@ export default function JourneyPage() {
   }
 
   return (
-    <JourneyDetails
-      baby={<BabyJourney weekNumber={week} />}
-      mom={<MomyJourney weekNumber={week} />}
-    />
+    <>
+      <JourneyDetails
+        baby={<BabyJourney weekNumber={week} />}
+        mom={<MomyJourney weekNumber={week} />}
+      />
+    </>
   );
 }
