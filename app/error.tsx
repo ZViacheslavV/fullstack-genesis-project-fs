cd3f00ff668202234137cfb9a254f811eab9a65d
Nata <skoropad_natalia@ukr.net>
2026-01-15 00:48:39 +0200

    add and style the error page

--- app/error.tsx
+++ app/error.tsx
@@ -1,23 +1,69 @@
 'use client';
 
-import ErrorState from '@/components/common/ErrorState/ErrorState';
+import Link from 'next/link';
+import Image from 'next/image';
+
+import Button from '@/components/common/Button/Button';
+import css from './error.module.css';
 
 //===========================================================================
 
 type Props = {
+  description: string;
   error: Error;
-  reset: () => void;
+  onRetry?: () => void;
 };
 
 //===========================================================================
 
-function GlobalError({ reset }: Props) {
+function GlobalError({ error, onRetry }: Props) {
   return (
-    <ErrorState
-      title="Помилка при завантаженні"
-      description="Щось пішло не так. Спробуйте, будь ласка, ще раз."
-      onRetry={reset}
-    />
+    <section className={css.page}>
+      <div className={css.left}>
+        <div className={css.logoRow}>
+          <Link href="/">
+            <Image
+              src="/company-logo.svg"
+              alt="Лелека"
+              width={105}
+              height={45}
+              priority
+            />
+          </Link>
+        </div>
+
+        <div className={css.formWrap}>
+          <div className={css.inner}>
+            <div className={css.content}>
+              <h2 className={css.title}>Помилка при завантаженні</h2>
+              <p className={css.desc}>
+                Ми вже розбираємось. Спробуйте повторити дію.
+              </p>
+
+              <p className={css.devMsg} title={error.message}>
+                {error.message}
+              </p>
+              <div className={css.actions}>
+                <Button onClick={onRetry} size="md">
+                  Спробувати знову
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={css.right} aria-hidden="true">
+        <Image
+          src={'/stork-with-nest-and-broken-eggs.jpg'}
+          alt=""
+          fill
+          priority
+          sizes="(min-width: 1440px) 40vw, 0px"
+          className={css.illustration}
+        />
+      </div>
+    </section>
   );
 }
 
