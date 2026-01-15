f63af7699fc01e7655b1a3bc1dc697222d8ebbc4
Vitaliy_Orlov <alpachino84@gmail.com>
2026-01-15 21:19:35 +0200

    fix stork loader and add baby loader gif

--- components/common/Loader/index.ts
+++ components/common/Loader/index.ts
@@ -1,2 +1,4 @@
 export { default as LoaderStork } from './LoaderStork';
-export { default as LoaderBaby } from './LoaderBaby';
+// export { default as LoaderBaby } from './LoaderBaby';
+export { default as InitialLoader } from './InitialLoader';
+export { default as Baby } from './Baby';
