8d14ccc2cb8ff9f7ae794959b4cdbb675a7c8033
Danylo <161326427+Daniel2-1@users.noreply.github.com>
2026-01-19 09:57:15 +0100

    removed excess toast check

--- app/(private routes)/(content-only)/profile/edit/OnboardingClient.tsx
+++ app/(private routes)/(content-only)/profile/edit/OnboardingClient.tsx
@@ -1,29 +1,15 @@
 'use client';
 
 import type { User } from '@/types/user';
-import toast from 'react-hot-toast';
 import AvatarPicker from '@/components/common/AvatarPicker/AvatarPicker';
 import OnboardingForm from '@/components/profile/OnboardingForm/OnboardingForm';
-import Toast from '@/components/common/Toast/Toast';
 
 interface Props {
   user: User;
 }
 
 export default function OnboardingClient({ user }: Props) {
-  const handleSuccess = () => {
-    toast.custom(
-      <Toast type="success" message="Профіль успішно оновлено" />,
-      { duration: 4000 }
-    );
-  };
 
-  const handleError = () => {
-    toast.custom(
-      <Toast type="error" message="Сталася помилка" />,
-      { duration: 5000 }
-    );
-  };
 
   return (
     <>
@@ -35,8 +21,6 @@ export default function OnboardingClient({ user }: Props) {
 
       <OnboardingForm
         initialData={user}
-        onSuccess={handleSuccess}
-        onError={handleError}
       />
     </>
   );
