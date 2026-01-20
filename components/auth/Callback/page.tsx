e5f8c04c616a110942b74629465a04adcc94af1c
Anna Polisciuc <annap00775@gmail.com>
2026-01-20 18:13:54 +0200

    add file callback.tsx

--- components/auth/Callback/page.tsx
+++ components/auth/Callback/page.tsx
@@ -1,31 +1,5 @@
-'use client';
+import Callback from './Callback';
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { checkSession } from '@/lib/api/clientApi';
-import LoaderBaby from '@/components/common/Loader/LoaderBaby';
-
-export default function GoogleAuthCallbackPage() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const initAuth = async () => {
-      try {
-        const success = await checkSession();
-
-        if (success) {
-          router.replace('/');
-        } else {
-          router.replace('/auth/login');
-        }
-      } catch (error) {
-        console.error('Google auth callback error', error);
-        router.replace('/auth/login');
-      }
-    };
-
-    initAuth();
-  }, [router]);
-
-  return <LoaderBaby />;
+export default function Page() {
+  return <Callback />;
 }
