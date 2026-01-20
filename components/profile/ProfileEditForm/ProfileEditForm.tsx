00df8e1e28005c49780214191b8a1bf679ed1d17
Yevheniia Serhieieva <y.o.serhieieva@gmail.com>
2026-01-20 12:39:03 +0200

    reset-password

--- components/profile/ProfileEditForm/ProfileEditForm.tsx
+++ components/profile/ProfileEditForm/ProfileEditForm.tsx
@@ -12,6 +12,7 @@ import { useRouter } from 'next/navigation';
 import css from './ProfileEditForm.module.css';
 import Toast from '@/components/common/Toast/Toast';
 import Modal from '@/components/common/Modal/Modal';
+import Button from '@/components/common/Button/Button';
 
 interface ProfileEditFormProps {
   user: User;
@@ -54,21 +55,21 @@ function ProfileEditForm({ user }: ProfileEditFormProps) {
     if (values.gender !== null) payload.gender = values.gender;
     if (values.dueDate) payload.dueDate = values.dueDate;
 
-    try {  setLoading(true);
+    try {
+      setLoading(true);
       await updateMe(payload);
       router.refresh();
       setToast({
-      message: 'Дані успішно збережено',
-      type: 'success',
-    });
+        message: 'Дані успішно збережено',
+        type: 'success',
+      });
     } catch (error) {
       console.error(error);
-        setToast({
-      message: 'Не вдалося зберегти зміни',
-      type: 'error',
-    });
-    }
-    finally {
+      setToast({
+        message: 'Не вдалося зберегти зміни',
+        type: 'error',
+      });
+    } finally {
       setLoading(false);
     }
   };
@@ -83,136 +84,161 @@ function ProfileEditForm({ user }: ProfileEditFormProps) {
       >
         {({ resetForm, errors, touched, dirty, submitForm, isValid }) => (
           <>
-          <Form>
-             {toast && (
-  <Toast message={toast.message} type={toast.type} />
-)}
-            <div className={css.inputFields}>
-
-              <label htmlFor={`${fieldId}-name`}  className={css.label}>
-                Ім`я
-                <Field
-                  id={`${fieldId}-name`}
-                  name="name"
-                  type="text"
-                   className={`${css.field} ${
-    touched.name && errors.name ? css.fieldError : ''
-  }`}
-                />
-                <ErrorMessage name="name" component="p" className={css.error}/>
-              </label>
-
-
-              <label htmlFor={`${fieldId}-email`} className={css.label}>
-                Пошта
-                <Field
-                  id={`${fieldId}-email`}
-                  name="email"
-                  type="email"
-                   className={`${css.field} ${
-    touched.email && errors.email ? css.fieldError : ''
-  }`}
-                />
-                <ErrorMessage name="email" component="p" className={css.error} />
-              </label>
-
-              <label htmlFor={`${fieldId}-gender`} className={css.label}>
-                Стать дитини
-                <Field name="gender" id={`${fieldId}-gender`}>
-                  {({ field, form }: FieldProps<FormValues['gender']>) => (
-                    <GenderSelect
-                      value={field.value}
-                      onChange={(value) =>
-                        form.setFieldValue(field.name, value)
-                      }
-                    />
-                  )}
-                </Field>
-                <ErrorMessage name="gender" component="p" className={css.error}/>
-              </label>
-
-              <label className={`${css.label} ${css.dateLabel}`}  htmlFor={`${fieldId}-dueDate`}>
-                Планова дата пологів
-
-                <div className={css.dateWrapper}>
+            <Form>
+              {toast && <Toast message={toast.message} type={toast.type} />}
+              <div className={css.inputFields}>
+                <label htmlFor={`${fieldId}-name`} className={css.label}>
+                  Ім`я
                   <Field
-                    id={`${fieldId}-dueDate`}
-                    name="dueDate"
-                    component={CalendarDatePicker}
-                    placeholderText="Оберіть дату"
-                    className={css.dateInput}
-                    autoComplete="off"
-                    onFocus={() => setIsDateFocused(true)}   
-                    onBlur={() => setIsDateFocused(false)}  
-                  />
-
-                  <span
-                    className={`${css.dateIcon} ${
-                      isDateFocused ? css.dateIconOpen : ''
+                    id={`${fieldId}-name`}
+                    name="name"
+                    type="text"
+                    className={`${css.field} ${
+                      touched.name && errors.name ? css.fieldError : ''
                     }`}
                   />
-                </div>
+                  <ErrorMessage
+                    name="name"
+                    component="p"
+                    className={css.error}
+                  />
+                </label>
 
-                <ErrorMessage name="dueDate" component="p" className={css.error}/>
-              </label>
+                <label htmlFor={`${fieldId}-email`} className={css.label}>
+                  Пошта
+                  <Field
+                    id={`${fieldId}-email`}
+                    name="email"
+                    type="email"
+                    className={`${css.field} ${
+                      touched.email && errors.email ? css.fieldError : ''
+                    }`}
+                  />
+                  <ErrorMessage
+                    name="email"
+                    component="p"
+                    className={css.error}
+                  />
+                </label>
 
+                <label htmlFor={`${fieldId}-gender`} className={css.label}>
+                  Стать дитини
+                  <Field name="gender" id={`${fieldId}-gender`}>
+                    {({ field, form }: FieldProps<FormValues['gender']>) => (
+                      <GenderSelect
+                        value={field.value}
+                        onChange={(value) =>
+                          form.setFieldValue(field.name, value)
+                        }
+                      />
+                    )}
+                  </Field>
+                  <ErrorMessage
+                    name="gender"
+                    component="p"
+                    className={css.error}
+                  />
+                </label>
 
-              <div className={css.buttonContainer}>
-                <button
-                  type="button"
-                  onClick={() => resetForm()}
-                  className={css.undoChanges}
+                <label
+                  className={`${css.label} ${css.dateLabel}`}
+                  htmlFor={`${fieldId}-dueDate`}
                 >
-                  {'Відмінити зміни'}
-                </button>
+                  Планова дата пологів
+                  <div className={css.dateWrapper}>
+                    <Field
+                      id={`${fieldId}-dueDate`}
+                      name="dueDate"
+                      component={CalendarDatePicker}
+                      placeholderText="Оберіть дату"
+                      className={css.dateInput}
+                      autoComplete="off"
+                      onFocus={() => setIsDateFocused(true)}
+                      onBlur={() => setIsDateFocused(false)}
+                    />
 
-                <button type="button" onClick={() => setIsConfirmOpen(true)}  disabled={!dirty || !isValid || loading} className={css.saveChanges}>
-                  
-                  {loading ? 'Завантаження...' : 'Зберегти зміни'}
-                </button>
+                    <span
+                      className={`${css.dateIcon} ${
+                        isDateFocused ? css.dateIconOpen : ''
+                      }`}
+                    />
+                  </div>
+                  <ErrorMessage
+                    name="dueDate"
+                    component="p"
+                    className={css.error}
+                  />
+                </label>
+
+                <div className={css.buttonContainer}>
+                  <button
+                    type="button"
+                    onClick={() => resetForm()}
+                    className={css.undoChanges}
+                  >
+                    {'Відмінити зміни'}
+                  </button>
+
+                  <button
+                    type="button"
+                    onClick={() => setIsConfirmOpen(true)}
+                    disabled={!dirty || !isValid || loading}
+                    className={css.saveChanges}
+                  >
+                    {loading ? 'Завантаження...' : 'Зберегти зміни'}
+                  </button>
+                </div>
               </div>
-            </div>
             </Form>
-             <Modal
-        isOpen={isConfirmOpen}
-        onClose={() => setIsConfirmOpen(false)}
-        closeOnBackdrop={!loading}
+
+            <div className={css.changePasswordWrapper}>
+              <Button
+                type="button"
+                className={css.changePasswordButton}
+                onClick={() => router.push('/auth/reset-password')}
+              >
+                Змінити пароль
+              </Button>
+            </div>
+
+            <Modal
+              isOpen={isConfirmOpen}
+              onClose={() => setIsConfirmOpen(false)}
+              closeOnBackdrop={!loading}
               closeOnEsc={!loading}
               modalClassName={css.modalSize}
-      >
-        <div className={css.modalPosition}>
-        <p className={css.modalText}>
-          Ви впевнені, що хочете зберегти внесені зміни?
-        </p>
+            >
+              <div className={css.modalPosition}>
+                <p className={css.modalText}>
+                  Ви впевнені, що хочете зберегти внесені зміни?
+                </p>
 
-        <div className={css.modalActions}>
-          <button
-            type="button"
-            className={css.undoChanges}
-            onClick={() => setIsConfirmOpen(false)}
-            disabled={loading}
-          >
-            Скасувати
-          </button>
+                <div className={css.modalActions}>
+                  <button
+                    type="button"
+                    className={css.undoChanges}
+                    onClick={() => setIsConfirmOpen(false)}
+                    disabled={loading}
+                  >
+                    Скасувати
+                  </button>
 
-          <button
-            type="button"
-            className={css.saveChanges}
-            disabled={loading}
-            onClick={async () => {
-            
-              await submitForm();
-                setIsConfirmOpen(false);
-            }}
-          >
-            Підтвердити
-          </button>
+                  <button
+                    type="button"
+                    className={css.saveChanges}
+                    disabled={loading}
+                    onClick={async () => {
+                      await submitForm();
+                      setIsConfirmOpen(false);
+                    }}
+                  >
+                    Підтвердити
+                  </button>
                 </div>
-                </div>
-      </Modal>
+              </div>
+            </Modal>
           </>
         )}
-        
       </Formik>
     </div>
   );
