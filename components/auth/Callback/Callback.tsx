15284b644a026e6fd79d26546e9a3061d35a72a1
Anna Polisciuc <annap00775@gmail.com>
2026-03-20 16:26:54 +0200

    fix google auth callback

--- components/auth/Callback/Callback.tsx
+++ components/auth/Callback/Callback.tsx
@@ -11,7 +11,14 @@ export default function Callback() {
   useEffect(() => {
     const initAuth = async () => {
       try {
-        const success = await checkSession();
+        let success = false;
+        for (let i = 0; i < 3; i++) {
+          success = await checkSession();
+          if (success) break;
+
+          await new Promise(res => setTimeout(res, 400));
+        }
+
         router.replace(success ? '/' : '/auth/login');
       } catch {
         router.replace('/auth/login');
